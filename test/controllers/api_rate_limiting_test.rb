5b8828d05278b962889959fb264e1c8fab167aa7
ArthurPV <arthurpontiervalois@gmail.com>
2025-07-02 23:32:41 +0200

    Fix rubocop.

--- test/controllers/api_rate_limiting_test.rb
+++ test/controllers/api_rate_limiting_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 # https://github.com/rails/rails/blob/b6963db965aa10ef9d63002f3a752cfcb20f0203/actionpack/test/controller/api/rate_limiting_test.rb#L5C1-L12C4
 class ApiRateLimitedController < ActionController::API
@@ -6,7 +6,7 @@ class ApiRateLimitedController < ActionController::API
   rate_limit to: 2, within: 2.seconds
 end
 
-class MessagesLimitedController < ApiRateLimitedController 
+class MessagesLimitedController < ApiRateLimitedController
   def limited_to_two_messages
     head :ok
   end
@@ -18,12 +18,12 @@ class UsersLimitedController < ApiRateLimitedController
   end
 end
 
-class ApiRateLimitingTest < ActionController::TestCase 
+class ApiRateLimitingTest < ActionController::TestCase
   setup do
     ApiRateLimitedController.cache_store.clear
   end
 
-  test 'should not limit messsages and users controller on the same rate limit count' do
+  test "should not limit messsages and users controller on the same rate limit count" do
     with_routing do |routing|
       routing.draw do
         get :limited_to_two_messages, to: "messages_limited#limited_to_two_messages"
